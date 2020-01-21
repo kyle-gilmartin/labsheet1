3c59eefe40924715814f9e0a1408327ba1603d11
s00188438 <s00188438@LAB-B1042-15>
2020-01-21 12:32:23 +0000

    changed class name to band

--- labsheet1/info.cs
+++ labsheet1/info.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace labsheet1
 {
-    class info:IComparable
+    public class Band:IComparable
     {
         public string BandName { get; set; }
         public DateTime Year { get; set; }
         public string Members { get; set; }
 
-        public info(string bandName, DateTime year, string members)
+        public Band(string bandName, DateTime year, string members)
         {
             BandName = bandName;
             Year = year;
